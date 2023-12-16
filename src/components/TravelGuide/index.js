53954cc62e368b81a49b68d5a10d5562810a7028
shanmukheswarikondeti <eswarikondeti@gmail.com>
2023-12-16 14:48:29 +0000

    travel guide

---
+++ src/components/TravelGuide/index.js
@@ -0,0 +1,95 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TravelPackage from '../TravelPackage'
+
+import {
+  TravelGuideContainer,
+  Heading,
+  PackagesContainer,
+  LoaderContainer,
+} from './styledComponents'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  inProgress: 'IN_PROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+
+class TravelGuide extends Component {
+  state = {apiStatus: apiStatusConstants.initial, travelPackages: []}
+
+  componentDidMount() {
+    this.getTravelPackages()
+  }
+
+  getTravelPackages = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+
+    const travelGuidePackagesApiUrl = `https://apis.ccbp.in/tg/packages`
+    const options = {method: 'GET'}
+    const travelPackagesResponse = await fetch(
+      travelGuidePackagesApiUrl,
+      options,
+    )
+    if (travelPackagesResponse.ok === true) {
+      const travelPackages = await travelPackagesResponse.json()
+      // console.log(travelPackages)
+      const formattedTravelPackages = travelPackages.packages.map(
+        eachPackage => ({
+          name: eachPackage.name,
+          id: eachPackage.id,
+          description: eachPackage.description,
+          imageUrl: eachPackage.image_url,
+        }),
+      )
+      this.setState({
+        apiStatus: apiStatusConstants.success,
+        travelPackages: formattedTravelPackages,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderLoader = () => (
+    <LoaderContainer data-testid="loader">
+      <Loader type="TailSpin" height={50} width={50} color="#52bbf0" />
+    </LoaderContainer>
+  )
+
+  renderPackages = () => {
+    const {travelPackages} = this.state
+    return (
+      <PackagesContainer>
+        {travelPackages.map(eachPackage => (
+          <TravelPackage packageDetails={eachPackage} key={eachPackage.id} />
+        ))}
+      </PackagesContainer>
+    )
+  }
+
+  renderApiStatusResults = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoader()
+      case apiStatusConstants.success:
+        return this.renderPackages()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <TravelGuideContainer>
+        <Heading>Travel Guide</Heading>
+        {this.renderApiStatusResults()}
+      </TravelGuideContainer>
+    )
+  }
+}
+
+export default TravelGuide
---
+++ src/components/TravelPackage/styledComponents.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const PackageItem = styled.li`
+  width: 35vw;
+  height: 50vh;
+  margin: 20px;
+  background-color: #ffffff;
+`
+
+export const PackageImage = styled.img`
+  height: 70%;
+  width: 100%;
+  margin: 0;
+`
+export const PackageDetails = styled.div`
+  padding: 10px;
+  margin: 0;
+`
+
+export const PackageName = styled.h1`
+    font-size:15px;
+    color:: #334155;
+    font-family:'Roboto';
+    margin-top:5px;
+    margin-bottom:5px;
+    font-weight:700;
+`
+
+export const PackageDesc = styled.p`
+  font-size: 12px;
+  font-family: 'Roboto';
+  color: #475569;
+`
